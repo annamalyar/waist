edf33f3a27986c0e3f12e20a50aa29ba6f6c0605
annamalyar <94036108+annamalyar@users.noreply.github.com>
2025-06-17 15:13:41 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-
   plugins: [react()],
   build: {
-    outDir: '../dist',
+    outDir: 'dist',         // ✅ правильный путь — внутри проекта
     emptyOutDir: true
   }
 });
